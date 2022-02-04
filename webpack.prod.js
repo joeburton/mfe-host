b38123ef2f072dc253cd9cb1ca8569e862b14541
Joe Burton <joeburton@gmail.com>
2022-02-04 09:52:28 +0000

    optomise built files

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
